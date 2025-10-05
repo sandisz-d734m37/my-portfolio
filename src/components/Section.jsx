f17fb48727ae92c76cc4e9e10b21e990a6cdd8eb
Sandisz Thieme <sandiszthieme@gmail.com>
2025-10-05 16:58:25 -0600

    Widen viewports

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,9 +1,9 @@
 export default function Section({ id, children }) {
   return (
-    <section id={id} className="w-full flex justify-center py-8 scroll-mt-24">
+    <section id={id} className="w-full flex justify-center py-1 scroll-mt-24">
       <div
-        className="custom_page bg-white dark:bg-neutral-800 w-[1080px] max-w-full mx-0 sm:mx-2 px-4 sm:px-5
-                   py-6 sm:py-8 transition-colors duration-500 border border-black dark:border-white"
+        className="custom_page bg-white dark:bg-neutral-800 w-[92%] max-w-full mx-0 sm:mx-2 px-1 sm:px-5
+                   py-6 sm:py-2 transition-colors duration-500 border border-black dark:border-white"
       >
         {children}
       </div>
