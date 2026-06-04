3075a01cb64361ad984a76d97d4fce7e28ed2fe1
Sandisz Thieme <sandiszthieme@gmail.com>
2026-06-04 11:42:46 -0600

    Add a bunch of graphics

--- src/components/GraphicsPage.jsx
+++ src/components/GraphicsPage.jsx
@@ -68,8 +68,72 @@ const photos = [
     alt: "Graphic or Logo reading D734M37 with star and sparkle overtop SVG",
   },
   {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/BLOCKs.svg",
+    alt: "Graphic design reading PPPT in 3d block letters SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/wreath_iguess.svg",
+    alt: "Military inspired antique wreath like graphic for Puppet Theater SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/ribbon.svg",
+    alt: "Puppet Theater staple Ribbon graphic initiated in Act 4 SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/shieldblue.svg",
+    alt: "Puppet Theater staple Shield graphic in Blue, initiated in Act 4 SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/longpt.svg",
+    alt: "Puppet Theater PT 2 letter long rendition of staple Cursive Signature logo SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/cursivo.svg",
+    alt: "Puppet Theater staple cursive signature logo, found on all tags starting in Act 4 SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/roundpt.svg",
+    alt: "Puppet Theater staple cursive PT logo, used where staple cursive is too long starting in Act 4 SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/f-corp.svg",
+    alt: "Puppet Theater Fantasy Corp. graphic found throughout Act 4 SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/big_u.svg",
+    alt: "Puppet Theater University Logo concept SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/too-tough.svg",
+    alt: "Puppet Theater The Puppets Too Tough To Die graphic concept SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/heep_hope_alive.svg",
+    alt: "Puppet Theater Keep Hope Alive graphic concept SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/adultsonly.svg",
+    alt: "Puppet Theater Adults Only graphic concept in my signature Cracked Up style SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/total_effing_p.svg",
+    alt: "Puppet Theater Total Fucking Puppet graphic with Death as a Jester in the background SVG",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/svgs/fucker-crackup.svg",
+    alt: "Personal Fucker, in my signature cracked up style SVG",
+  },
+  {
     src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/pngs/kumri_graphic.png",
     alt: "Graphic featuring Kumri Akhmedova, Hero of Socialist Labor",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/pngs/i_love_radio.png",
+    alt: "Jacket concept featuring Puppet Theater signature cursive logo and I Love The Radio patches",
+  },
+  {
+    src: "https://sthieme-portfolio-assets.s3.us-east-2.amazonaws.com/graphics/pngs/give_me_back.png",
+    alt: "Graphic concept featuring a gun and phrase give me back my bullets in my signature cracked up style, inspired by vintage Entombed tee",
   },
 ];
 
@@ -77,7 +141,7 @@ export default function PhotographyHomePage() {
   return (
     <div>
       <h2 className="text-4xl font-semibold mb-6">Graphics</h2>
-      <div class="columns-2 gap-2 space-y-2">
+      <div class="columns-2 gap-4 space-y-4">
         {[...photos]
           .sort(() => Math.random() - 0.5)
           .map((photo, index) => (
